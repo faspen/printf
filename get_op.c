c996e3d8560fcc992febe3b7afc042c1066c9b55
Ben Dosch <Benjamindosch@gmail.com>
2020-10-24 21:17:53 +0000

    hope this resolves the confilct

--- get_op.c
+++ get_op.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
-  * get_op - retrieve appropriate function
-  *
-  * @format: specified function
-  * @valist: variadic stuff
-  *
-  * Return: function
-  */
+ * get_op - retrieve appropriate function
+ *
+ * @format: specified function
+ * @valist: variadic stuff
+ *
+ * Return: function
+ */
 
 int get_op(char *format, va_list valist)
 {
@@ -16,4 +16,31 @@ int get_op(char *format, va_list valist)
 		{"d", _print_int},
 		{"i", _print_int}
 	};
+}
+
+/**
+ * get_cp_func - string case
+ *
+ * @c: location in format
+ * @arg: va_arg in printf.c
+ *
+ * Return: count
+ */
+int get_cp_func(char *c, char *arg)
+{
+	int i = 0;
+
+	switch (*c)
+	{
+	case 's':
+	{
+		while (arg[i] != '\0')
+		{
+			_putchar(arg[i]);
+			i++;
+		}
+		return (i);
+	}
+	}
+	return (NULL);
 }
