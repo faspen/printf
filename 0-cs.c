92f9e30d317be2d78c1d85466d716fcd852d099e
faspen <2335@holbertonschool.com>
2020-10-24 14:11:03 +0000

    task 0

---
+++ 0-cs.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+  * printf - print something
+  * @format: character string
+  * Return: number of characters printed
+  */
+
+int _printf(const char *format, ...)
