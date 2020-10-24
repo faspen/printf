01750a676bf9adc5a7233e0e8082bfbd7ac0b8ff
faspen <2335@holbertonschool.com>
2020-10-24 14:56:45 +0000

    task 0

--- 0-printf.c
+++ 0-printf.c
@@ -2,8 +2,14 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
-  *
+  * _printf - print something
+  * @format: character string
+  * Return: number of characters printed
   */
 
 int _printf(const char *format, ...)
 {
+	unsigned int i;
+	va_list valist;
+
+	va_start(valist, format);
